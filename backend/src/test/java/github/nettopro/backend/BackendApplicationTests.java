e970fe92fef97a993c82338f9420c5c20ab34ebe
Netto Pro <enzomnf_pro@outlook.com>
2025-05-26 22:16:58 -0300

    Começando desenvolvimento do RPGenzo Iniciando todo o processo de setup do local de desenvolvimento, com Docker, Maven, Spring Boot e PostgreSQL

---
+++ backend/src/test/java/github/nettopro/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package github.nettopro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
