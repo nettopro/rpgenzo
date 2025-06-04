4237bb7e728a1715cf7c14efc3b3b064ae857907
Netto Pro <enzomnf_pro@outlook.com>
2025-06-04 13:06:55 -0300

    remocao do uso de forms e migrado criacao pro rest Decidi cancelar o uso de Forms e Controller e usar REST para a criação de tipos. Deixando mais conciso o código e melhor para quando eu for criar o front-end.

--- backend/src/main/java/github/nettopro/rpgenzo/controller/acoes/TipoRestController.java
+++ backend/src/main/java/github/nettopro/rpgenzo/controller/acoes/TipoRestController.java
@@ -4,21 +4,36 @@ import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import github.nettopro.rpgenzo.model.acoes.Tipo;
 import github.nettopro.rpgenzo.service.acoes.TipoService;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/tipo")
+@RequestMapping("/api/tipos")
 public class TipoRestController {
     private final TipoService tipoService;
 
     public TipoRestController(TipoService tipoService) {
         this.tipoService = tipoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criarTipo(@Valid @RequestBody Tipo tipo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+
+        tipoService.criarESalvarTipo(tipo);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Tipo criado com sucesso");
     }
 
     @DeleteMapping("/{id}")
