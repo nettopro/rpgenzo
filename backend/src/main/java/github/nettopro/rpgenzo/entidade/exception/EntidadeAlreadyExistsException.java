8752ef6df19366453de9404be259e1d9bdd99b7b
Netto Pro <enzomnf_pro@outlook.com>
2025-07-15 11:00:02 -0300

    removido Getter inutilizado na exceção

--- backend/src/main/java/github/nettopro/rpgenzo/entidade/exception/EntidadeAlreadyExistsException.java
+++ backend/src/main/java/github/nettopro/rpgenzo/entidade/exception/EntidadeAlreadyExistsException.java
@@ -1,8 +1,5 @@
 package github.nettopro.rpgenzo.entidade.exception;
 
-import lombok.Getter;
-
-@Getter
 public class EntidadeAlreadyExistsException extends RuntimeException {
 
     public EntidadeAlreadyExistsException(String message) {
