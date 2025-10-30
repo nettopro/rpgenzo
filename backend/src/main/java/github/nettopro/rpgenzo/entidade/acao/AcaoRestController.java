f5dc3057fd1e2ee0850e5898f13a2d8214069c54
Netto Pro <enzomnf_pro@outlook.com>
2025-10-30 18:53:10 -0300

    adicionado metodo CORS no backend

--- backend/src/main/java/github/nettopro/rpgenzo/entidade/acao/AcaoRestController.java
+++ backend/src/main/java/github/nettopro/rpgenzo/entidade/acao/AcaoRestController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,12 +46,14 @@ public class AcaoRestController {
         return ResponseEntity.noContent().build();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/sem-tipo/{id}")
     public ResponseEntity<Optional<AcaoSemTipoResponse>> buscarAcaoSemTipoPorId(@PathVariable("id") Integer id) {
         Optional<AcaoSemTipoResponse> acaoResponse = acaoService.buscarAcaoSemTipoPorId(id);
         return ResponseEntity.ok(acaoResponse);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/com-tipo/{id}")
     public ResponseEntity<Optional<AcaoComNomeDoTipoResponse>> buscarAcaoComNomeDoTipoPorId(@PathVariable("id") Integer id) {
         Optional<AcaoComNomeDoTipoResponse> acaoResponse = acaoService.buscarAcaoComNomeDoTipoPorId(id);
