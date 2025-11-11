6a99791f8a7bf888216c6cca8e8a5d30f3645843
Netto Pro <enzomnf_pro@outlook.com>
2025-11-11 18:31:11 -0300

    filtro de nome e pontos de acao feito

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, DestroyRef, inject, model, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AcaoService } from './lista-acao/acao/acao.service';
 import { Acao } from './lista-acao/acao/acao.model';
@@ -15,8 +15,11 @@ export class App implements OnInit {
   protected title = 'frontend';
 
   private acaoService = new AcaoService();
-  protected acoes = signal<Acao[]>([]);
+  acoes = signal<Acao[]>([]);
   private destroyRef = inject(DestroyRef);
+
+  textoFiltro = signal<string>('');
+  filtroPontosAcao = signal<number>(1);
 
   ngOnInit() {
     const subscription = this.acaoService.getTodasAcoes().subscribe(
@@ -34,4 +37,12 @@ export class App implements OnInit {
       subscription.unsubscribe();
     });
   }
+
+  acoesFiltradas = computed(() => {
+  const filtro = this.textoFiltro().toLowerCase();
+  return this.acoes().filter(acao => 
+    acao.nome.toLowerCase().includes(filtro) && 
+    acao.acaoCusto === this.filtroPontosAcao()
+  );
+});
 }
--- frontend/src/app/filtro/filtro.ts
+++ frontend/src/app/filtro/filtro.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, model, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filtro',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filtro.html',
   styleUrl: './filtro.scss',
 })
 export class Filtro {
+  filtro = model<string>();
 
+  pontoAcaoFiltro = model<number>();
 }
